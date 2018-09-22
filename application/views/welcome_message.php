006f5bb7a7cf75587bfadf9d294bee5bb230ee14
Unknown <huggigames@googlemail.com>
2018-09-22 17:20:21 -0400

    Test firebase Change

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,6 +13,88 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <!-- Bulma Version 0.7.1-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css" />
+        <script src="firebase.js"></script>
+<script type="text/javascript">
+    var config={
+    apiKey:"AIzaSyC5EuoydXwtQjjibe0e093t_fvfZaOnnwY",
+    authDomain: "rvabucks.firebaseapp.com",
+    databaseURL: "https://rvabucks.firebaseio.com",
+    storageBucket: "rvabucks.appspot.com"
+};
+var globalUserName="Yousef";
+var userData;
+ firebase.initializeApp(config);
+var defaultDatabase=firebase.database();
+function newUser(userId,password)
+{
+    firebase.database().ref('users/'+userId).set({ID:userId,EXP:0,TIER:1,PASSWORD:"dummypass",BALANCE:0});
+    globalUserName=userId;
+
+}
+function changeBalance(quantity)
+{
+    var balanceRef= firebase.database().ref("users");
+    var balance=0;
+    firebase.database().ref('/users/'+globalUserName).once("value").then(function(snapshot){
+        balance=snapshot.val().BALANCE;
+        var id=snapshot.val().ID;
+        var exp=snapshot.val().EXP;
+        var tier=snapshot.val().TIER;
+        var password=snapshot.val().PASSWORD;
+
+        console.log(balance);
+
+        var newBalance=balance+quantity;
+        var postData={ID:id,EXP:0,TIER:1,PASSWORD:"dummypass",BALANCE:newBalance};
+    var updates={};
+    updates['/'+globalUserName+'/']=postData;
+    firebase.database().ref('users').update(updates);
+
+    });
+    
+}
+function changeExp(newexp)
+{
+    var expRef= firebase.database().ref("users");
+    var exp=0;
+    firebase.database().ref('/users/'+globalUserName).once("value").then(function(snapshot){
+        exp=snapshot.val().EXP;
+        var id=snapshot.val().ID;
+        var balance=snapshot.val().BALANCE;
+        var tier=snapshot.val().TIER;
+        var password=snapshot.val().PASSWORD;
+
+        console.log(balance);
+
+        var updatedEXP=exp+newexp;
+        if(updatedEXP>10)
+        {
+            tier+=Math.floor(updatedEXP/10);
+            updatedEXP=updatedEXP%10;
+            var postData={ID:id,EXP:updatedEXP,TIER:tier,PASSWORD:"dummypass",BALANCE:balance};
+        }
+        else{
+        var postData={ID:id,EXP:updatedEXP,TIER:1,PASSWORD:"dummypass",BALANCE:balance};
+    }
+    var updates={};
+    updates['/'+globalUserName+'/']=postData;
+    firebase.database().ref('users').update(updates);
+
+    });
+}
+function returnUserJSON()
+{
+firebase.database().ref('users/'+globalUserName).once("value").then(function(snapshot){
+userData=snapshot.val();
+console.log(userData);
+updateUI();
+});
+}
+function updateUI()
+{
+document.getElementById("money").innerHTML=userData.BALANCE;
+}
+</script>
     <style>
     
     .navbar {
@@ -76,9 +158,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 }
 
 </style>
+
 </head>
 
-<body>
+<body onload="returnUserJSON()">
     <div class="navbar is-inline-flex is-transparent">
         <div class="navbar-brand">
             <a class="navbar-item">
@@ -117,7 +200,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </div>
 
                 <div class="card-content">
-                    <div class="money">$10.00</div>
+                    <div id="money" class="money">$10.00</div>
 
                     <a style="margin-right:20px;" class="button is-large is-info">Pay</a><a class="button is-info is-large">Deposit</a>
                 </div>
