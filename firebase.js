68e35acb39df2db4a3416d62df1d7e86da1bfc58
Unknown <huggigames@googlemail.com>
2018-09-22 14:00:49 -0400

    Adding Config Info

---
+++ firebase.js
@@ -0,0 +1,9 @@
+var config={
+	apiKey:"AIzaSyC5EuoydXwtQjjibe0e093t_fvfZaOnnwY",
+	authDomain: "rvabucks.firebaseapp.com",
+    databaseURL: "https://rvabucks.firebaseio.com",
+    projectId: "rvabucks",
+    storageBucket: "rvabucks.appspot.com",
+    messagingSenderId: "981964597054"
+};
+firebaseapp.initializeApp(config);
